b98f9cb991ad3a32bdc5224a2660df8e867bb622
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-12 13:09:37 +0300

    homecarousel slider datas

---
+++ src/assets/assets.js
@@ -0,0 +1,5 @@
+import headerSlider from "../assets/header-slider-shop.svg";
+
+export const assets = {
+  sliders: [headerSlider],
+};
---
+++ src/data.js
@@ -0,0 +1,13 @@
+import { assets } from "./assets/assets";
+export const data = {
+  slider: [
+    {
+      term: "SUMMER 2020",
+      title: "NEW COLLECTION",
+      description:
+        "We know how large objects will act, but things on a small scale.",
+      button: "SHOP NOW",
+      image: assets.sliders[0],
+    },
+  ],
+};
