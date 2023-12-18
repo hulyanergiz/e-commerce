6e540c903c9ba075c722e37a199b2c5c4808c55d
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-19 01:41:11 +0300

    merge

--- src/components/product/productNavigation.js
+++ src/components/product/productNavigation.js
@@ -3,11 +3,7 @@ import { NavLink } from "reactstrap";
 const ProductNavigation = () => {
   return (
     <div className="w-full flex justify-center">
-<<<<<<< HEAD
       <div className="w-[30%] flex justify-between text-[#737373] text-sm font-bold no-underline py-6">
-=======
-      <div className="w-[35%] flex justify-between text-[#737373] text-sm font-bold no-underline py-6">
->>>>>>> 6518c110d1c654843e7ed44fb360c3ab2ac8ef39
         <NavLink href="/">Description</NavLink>
         <NavLink href="/shop">Additional Information</NavLink>
         <NavLink href="#">
