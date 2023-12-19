932e8a5d1c5799ead338fe60dc95a90cf0d4fb11
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-19 23:10:50 +0300

    creates the component& adds to AboutUs page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
+import AboutProblemsText from "../components/about/AboutProblemsText";
 import AboutTop from "../components/about/AboutTop";
 
 const AboutUs = () => {
   return (
     <div className="w-full flex">
-      <div className="w-[73%] flex flex-row m-auto">
+      <div className="w-[73%] flex flex-col m-auto">
         <AboutTop />
+        <AboutProblemsText />
       </div>
     </div>
   );
