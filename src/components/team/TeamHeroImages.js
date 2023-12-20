d8271f8399276747ab0150f3f5ff2e4aa22e9680
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-20 12:56:43 +0300

    css revision

--- src/components/team/TeamHeroImages.js
+++ src/components/team/TeamHeroImages.js
@@ -2,15 +2,15 @@ import { data } from "../../data";
 
 const TeamHeroImages = () => {
   return (
-    <div className="w-full flex flex-row gap-2">
-      <img src={data.teamHeroImages[0]} />
-      <div className="flex flex-col gap-2">
-        <img src={data.teamHeroImages[1]} />
-        <img src={data.teamHeroImages[2]} />
+    <div className="w-full flex flex-row gap-4">
+      <img src={data.teamHeroImages[0]} className="flex w-[49%]" alt="hero" />
+      <div className="flex flex-col gap-4 w-[25%]">
+        <img src={data.teamHeroImages[1]} alt="hero" />
+        <img src={data.teamHeroImages[2]} alt="hero" />
       </div>
-      <div className="flex flex-col gap-2">
-        <img src={data.teamHeroImages[3]} />
-        <img src={data.teamHeroImages[4]} />
+      <div className="flex flex-col gap-4 w-[25%]">
+        <img src={data.teamHeroImages[3]} alt="hero" />
+        <img src={data.teamHeroImages[4]} alt="hero" />
       </div>
     </div>
   );
