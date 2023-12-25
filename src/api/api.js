a74e7363832a69b38a07a4123f54417ca3765d79
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-25 20:15:23 +0300

    baseurl editted

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const createAxiosInstance = () => {
   return axios.create({
-    baseURL: "https://workintech-fe-ecommerce.onrender.com/",
+    baseURL: "https://workintech-fe-ecommerce.onrender.com",
     headers: {},
   });
 };
