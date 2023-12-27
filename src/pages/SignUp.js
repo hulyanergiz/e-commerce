1004f7c259a6965d540b4474c44931fa77893936
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-27 17:01:21 +0300

    adds css styling to sign up form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -70,189 +70,224 @@ const SignUp = () => {
   return (
     <form
       onSubmit={handleSubmit(submitHandler)}
-      className="flex flex-col gap-4 items-center"
+      className="flex  flex-col items-center m-auto"
     >
-      <h1 className="text-2xl font-bold">Sign Up</h1>
-      <label>
-        Name:
-        <input
-          className="border-solid border-1 border-[#737373] rounded-[5px]"
-          type="text"
-          {...register("name", {
-            required: "Name is required",
-            minLength: {
-              value: 3,
-              message: "Must be at least three characters long.",
-            },
-          })}
-        />
-      </label>
-      {errors.name && <p>{errors.name.message}</p>}
-      <label>
-        Email:
-        <input
-          className="border-solid border-1 border-[#737373] rounded-[5px]"
-          type="email"
-          {...register("email", {
-            required: "Email is required.",
-            pattern: {
-              value: emailRegex,
-              message: "Must be a valid email address.",
-            },
-          })}
-        />
-      </label>
-      {errors.email?.message && <p>{errors.email.message}</p>}
-      <label>
-        Password:
-        <input
-          className="border-solid border-1 border-[#737373] rounded-[5px]"
-          type={togglePass1 ? "text" : "password"}
-          {...register("password", {
-            required: "Password is required",
-            minLength: {
-              value: 6,
-              message: "Must be at least 6 characters long.",
-            },
-          })}
-        />
-        {togglePass1 ? (
-          <FontAwesomeIcon
-            icon={faLock}
-            onClick={() => setTogglePass1(!togglePass1)}
-          />
-        ) : (
-          <FontAwesomeIcon
-            icon={faEye}
-            onClick={() => setTogglePass1(!togglePass1)}
-          />
-        )}
-      </label>
-      {errors.password && <p>{errors.password.message}</p>}
-      <label>
-        Confirm Password:
-        <input
-          className="border-solid border-1 border-[#737373] rounded-[5px]"
-          type={togglePass2 ? "text" : "password"}
-          {...register("confirmPassword", {
-            required: "Password is required",
-            validate: (value) =>
-              value === getValues("password") || "Passwords are not matched",
-          })}
-        />
-        {togglePass2 ? (
-          <FontAwesomeIcon
-            icon={faLock}
-            onClick={() => setTogglePass2(!togglePass2)}
-          />
-        ) : (
-          <FontAwesomeIcon
-            icon={faEye}
-            onClick={() => setTogglePass2(!togglePass2)}
-          />
-        )}
-      </label>
-      {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
-      <div>
-        <select id="role_id" {...register("role_id")}>
-          {roles.map((role, i) => (
-            <option key={i} value={role.id}>
-              {role.code}
-            </option>
-          ))}
-        </select>
-      </div>
+      <h1 className="text-2xl font-bold">
+        Register <h3 className="text-xl"> Sign Up</h3>
+      </h1>
 
-      {watch("role_id") === "2" && (
-        <>
-          <label>
-            Store Name:
+      <div className="flex flex-col gap-4 items-center w-[30%]">
+        <div className="label-input">
+          <label htmlFor="name">Name:</label>
+          <input
+            className="input"
+            type="text"
+            {...register("name", {
+              required: "Name is required",
+              minLength: {
+                value: 3,
+                message: "Must be at least three characters long.",
+              },
+            })}
+          />
+        </div>
+
+        {errors.name && <p className="error">{errors.name.message}</p>}
+        <div className="label-input">
+          <label htmlFor="email">Email:</label>
+          <input
+            className="input"
+            type="email"
+            {...register("email", {
+              required: "Email is required.",
+              pattern: {
+                value: emailRegex,
+                message: "Must be a valid email address.",
+              },
+            })}
+          />
+        </div>
+
+        {errors.email?.message && (
+          <p className="error">{errors.email.message}</p>
+        )}
+        <div className="label-input">
+          <label htmlFor="password">Password:</label>
+          <div className="input-icon">
             <input
-              className="border-solid border-1 border-[#737373] rounded-[5px]"
-              type="text"
-              id="name"
-              {...register("name", {
-                required: "Store name is required",
+              className="input"
+              type={togglePass1 ? "text" : "password"}
+              {...register("password", {
+                required: "Password is required",
                 minLength: {
-                  value: 3,
-                  message: "Must be at least three characters long.",
+                  value: 6,
+                  message: "Must be at least 6 characters long.",
                 },
               })}
             />
-          </label>
-          {errors.name && <p>{errors.name.message}</p>}
-          <label>
-            Store Phone:
+            <div className="-ml-14">
+              {togglePass1 ? (
+                <FontAwesomeIcon
+                  size="xl"
+                  icon={faLock}
+                  onClick={() => setTogglePass1(!togglePass1)}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  size="xl"
+                  icon={faEye}
+                  onClick={() => setTogglePass1(!togglePass1)}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+
+        {errors.password && <p className="error">{errors.password.message}</p>}
+        <div className="label-input">
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <div className="input-icon">
             <input
-              className="border-solid border-1 border-[#737373] rounded-[5px]"
-              type="tel"
-              {...register("phone", {
-                required: "Store phone is required",
-                pattern: {
-                  value: turkeyPhoneRegex,
-                  message: "Must be a valid phone number",
-                },
+              className="input"
+              type={togglePass2 ? "text" : "password"}
+              {...register("confirmPassword", {
+                required: "Password is required",
+                validate: (value) =>
+                  value === getValues("password") ||
+                  "Passwords are not matched",
               })}
             />
-          </label>
-          {errors.phone && <p>{errors.phone.message}</p>}
-          <label>
-            Store Tax ID:
-            <input
-              className="border-solid border-1 border-[#737373] rounded-[5px]"
-              type="text"
-              {...register("tax_no", {
-                required: "Store Tax ID is required",
-                pattern: {
-                  value: /^T\d{4}V\d{6}$/,
-                  message:
-                    "Must be a valid tax ID in this format: TXXXXVXXXXXX",
-                },
-              })}
-            />
-          </label>
-          {errors.tax_no && <p>{errors.tax_no.message}</p>}
-          <label>
-            Store Bank Account:
-            <input
-              className="border-solid border-1 border-[#737373] rounded-[5px]"
-              type="text"
-              {...register("bank_account", {
-                required: "Store Bank Account is required",
-                pattern: {
-                  value: ibanRegex,
-                  message: "Must be a valid store bank account",
-                },
-              })}
-            />
-          </label>
-          {errors.bank_account && <p>{errors.bank_account.message}</p>}
-        </>
-      )}
-      {!isLoading ? (
-        <button
-          type="submit"
-          disabled={!isValid}
-          className={
-            !isValid
-              ? "bg-[#23A6F0] text-white p-3 rounded-[5px] w-[150px]"
-              : " bg-[#23856D] text-white p-3 rounded-[5px] w-[150px] cursor-pointer"
-          }
-        >
-          Sign Up
-        </button>
-      ) : (
-        <>
-          <PulseLoader />
+            <div className="-ml-14">
+              {togglePass2 ? (
+                <FontAwesomeIcon
+                  size="xl"
+                  icon={faLock}
+                  onClick={() => setTogglePass2(!togglePass2)}
+                />
+              ) : (
+                <FontAwesomeIcon
+                  size="xl"
+                  icon={faEye}
+                  onClick={() => setTogglePass2(!togglePass2)}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+
+        {errors.confirmPassword && (
+          <p className="error">{errors.confirmPassword.message}</p>
+        )}
+        <div className="flex flex-row items-center gap-4 text-xl justify-center py-1 px-2 border-solid border-2  border-[#737373] rounded-md w-[150px]">
+          <select
+            id="role_id"
+            value={watch("role_id")}
+            {...register("role_id")}
+          >
+            {roles.map((role, i) => (
+              <option key={i} value={role.id}>
+                {role.code}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {watch("role_id") === "2" && (
+          <>
+            <div className="label-input">
+              <label htmlFor="name">Store Name:</label>
+              <input
+                className="input"
+                type="text"
+                id="name"
+                {...register("name", {
+                  required: "Store name is required",
+                  minLength: {
+                    value: 3,
+                    message: "Must be at least three characters long.",
+                  },
+                })}
+              />
+            </div>
+            {errors.name && <p>{errors.name.message}</p>}
+            <div className="label-input">
+              <label htmlFor="phone">Store Phone:</label>
+              <input
+                className="input"
+                type="tel"
+                {...register("phone", {
+                  required: "Store phone is required",
+                  pattern: {
+                    value: turkeyPhoneRegex,
+                    message: "Must be a valid phone number",
+                  },
+                })}
+              />
+            </div>
+
+            {errors.phone && <p className="error">{errors.phone.message}</p>}
+            <div className="label-input">
+              <label htmlFor="tax_no">Store Tax ID:</label>
+              <input
+                className="input"
+                type="text"
+                {...register("tax_no", {
+                  required: "Store Tax ID is required",
+                  pattern: {
+                    value: /^T\d{4}V\d{6}$/,
+                    message:
+                      "Must be a valid tax ID in this format: TXXXXVXXXXXX",
+                  },
+                })}
+              />
+            </div>
+
+            {errors.tax_no && <p className="error">{errors.tax_no.message}</p>}
+            <div className="label-input">
+              <label htmlFor="bank_account">Store Bank Account:</label>
+              <input
+                className="input"
+                type="text"
+                {...register("bank_account", {
+                  required: "Store Bank Account is required",
+                  pattern: {
+                    value: ibanRegex,
+                    message: "Must be a valid store bank account",
+                  },
+                })}
+              />
+            </div>
+
+            {errors.bank_account && (
+              <p className="error">{errors.bank_account.message}</p>
+            )}
+          </>
+        )}
+        {!isLoading ? (
           <button
             type="submit"
-            disabled={isLoading}
-            className={" bg-[#23A6F0] text-white p-3 rounded-[5px] w-[150px]"}
+            disabled={!isValid}
+            className={
+              !isValid
+                ? "bg-[#23A6F0] text-white p-3 rounded-[5px] w-[150px]"
+                : " bg-[#23856D] text-white p-3 rounded-[5px] w-[150px] cursor-pointer"
+            }
           >
-            Signing Up
+            Sign Up
           </button>
-        </>
-      )}
+        ) : (
+          <>
+            <PulseLoader />
+            <button
+              type="submit"
+              disabled={isLoading}
+              className={" bg-[#23A6F0] text-white p-3 rounded-[5px] w-[150px]"}
+            >
+              Signing Up
+            </button>
+          </>
+        )}
+      </div>
     </form>
   );
 };
