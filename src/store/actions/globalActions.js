50fddf0b22be8b193801a0edbf867c862cccf38f
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-28 23:14:37 +0300

    creates globalActions

---
+++ src/store/actions/globalActions.js
@@ -0,0 +1,15 @@
+export const SET_ROLES = "SET_ROLES";
+export const SET_CATEGORIES = "SET_CATEGORIES";
+export const SET_THEME = "SET_THEME";
+export const SET_LANGUAGE = "SET_LANGUAGE";
+
+export const setRoles = (roles) => ({ type: SET_ROLES, payload: roles });
+export const setCategories = (categories) => ({
+  type: categories,
+  payload: categories,
+});
+export const setTheme = (theme) => ({ type: SET_THEME, payload: theme });
+export const setLanguage = (language) => ({
+  type: SET_LANGUAGE,
+  payload: language,
+});
