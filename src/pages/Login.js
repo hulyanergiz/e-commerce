7cedf35d58190faa9f1e5137259b47bf29ad40ee
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-29 00:26:09 +0300

    creates login page template

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+const Login = () => {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({ mode: "all" });
+  return (
+  );
+};
+export default Login;
