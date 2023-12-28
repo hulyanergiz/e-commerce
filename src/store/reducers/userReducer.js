14c1240ba405cbe07dcda440a34ac538597d7175
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-28 22:59:47 +0300

    adds set_user case to user reducer

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,7 +1,9 @@
-const initialState = {};
+const initialState = { user: {} };
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "SET_USER":
+      return { ...state, user: action.payload };
     default:
       return state;
   }
