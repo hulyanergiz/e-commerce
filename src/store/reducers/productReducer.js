c3195cdafe2006461a08526ee6e30f07cae00326
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-28 23:29:00 +0300

    creates productReducer

--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -1,7 +1,31 @@
-const initialState = {};
+import {
+  SET_ACTIVE_PAGE,
+  SET_FETCH_STATE,
+  SET_PAGE_COUNT,
+  SET_PRODUCT_LIST,
+  SET_TOTAL_PRODUCT_COUNT,
+} from "../actions/productActions";
+
+const initialState = {
+  productList: [],
+  totalProductCount: 0,
+  pageCount: 0,
+  activePage: 0,
+  fetchState: "",
+};
 
 export const productReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_PRODUCT_LIST:
+      return { ...state, productList: action.payload };
+    case SET_TOTAL_PRODUCT_COUNT:
+      return { ...state, totalProductCount: action.payload };
+    case SET_PAGE_COUNT:
+      return { ...state, pageCount: action.payload };
+    case SET_ACTIVE_PAGE:
+      return { ...state, activePage: action.payload };
+    case SET_FETCH_STATE:
+      return { ...state, fetchState: action.payload };
     default:
       return state;
   }
