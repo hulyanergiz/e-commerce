cccd6694d3db1ea23faac8e6d409187913d72dfa
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-28 23:46:03 +0300

    creates storeReducer

--- src/store/reducers/storeReducer.js
+++ src/store/reducers/storeReducer.js
@@ -1,7 +1,11 @@
-const initialState = {};
+import { SET_STORE } from "../actions/storeActions";
+
+const initialState = { store: {} };
 
 export const storeReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_STORE:
+      return { ...state, store: action.payload };
     default:
       return state;
   }
