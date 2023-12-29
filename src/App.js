e293dfdc6a59426ccb9ad69adb75009e5283eaca
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-29 17:34:27 +0300

    get axios to /verify path

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import "./App.css";
 import Header from "./layouts/Header";
 import PageContent from "./layouts/PageContent";
 import Footer from "./layouts/Footer";
+import { useDispatch } from "react-redux";
+import { AxiosInstance, renewAxiosInstance } from "./api/api";
+import { setUser } from "./store/actions/userActions";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 function App() {
+  const dispatch = useDispatch();
+  const tokenExist = localStorage.getItem("token");
+  const history = useHistory();
+
+  useEffect(() => {
+    if (tokenExist) {
+      AxiosInstance.get("/verify")
+        .then((res) => {
+          console.log("verify", res);
+          const userData = res.data;
+          dispatch(setUser(userData));
+          renewAxiosInstance();
+          history.go(-1);
+        })
+        .catch((err) => {
+          console.error("verify", err);
+          localStorage.removeItem("token");
+          renewAxiosInstance();
+        });
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header />
