219e468488efc5ccbcc7296fd9052cb263dc7fed
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-02 23:40:47 +0300

    adds user login actions

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,4 +1,40 @@
-export const setUser = (user) => ({
-  type: "SET_USER",
+import { toast } from "react-toastify";
+import { AxiosInstance, renewAxiosInstance } from "../../api/api";
+
+export const SET_USER_LOGIN_SUCCESS = "SET_USER_LOGIN_SUCCESS";
+export const SET_USER_LOGIN_FAILURE = "SET_USER_LOGIN_FAILURE";
+export const SET_USER_LOGGED_OUT = "SET_USER_LOGGED_OUT";
+
+export const setUserSuccess = (user) => ({
+  type: SET_USER_LOGIN_SUCCESS,
   payload: user,
 });
+
+export const setUserFailure = (error) => ({
+  type: SET_USER_LOGIN_FAILURE,
+  payload: error,
+});
+export const removeUser = () => ({
+  type: SET_USER_LOGGED_OUT,
+});
+
+export const userLogin =
+  (formData, history, setToken, setName) => (dispatch) => {
+    AxiosInstance.post("/login", formData)
+      .then((res) => {
+        console.log("Login post:", res.data);
+        dispatch(setUserSuccess(res.data));
+        setToken(res.data.token);
+        setName(res.data.name);
+        toast.success("Successfull login!");
+        setTimeout(() => {
+          history.push("/");
+        }, 2000);
+        renewAxiosInstance();
+      })
+      .catch((err) => {
+        console.error("Login error:", "Error occurred");
+        dispatch(setUserFailure(err));
+        toast.error(err.error);
+      });
+  };
