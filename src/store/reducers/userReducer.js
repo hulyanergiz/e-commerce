75b6f6fcbd425e5e900f2b8d23e0601737d93c41
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-02 23:41:36 +0300

    updates states according to user actions in userReducer

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -1,9 +1,29 @@
-const initialState = { user: {} };
+import {
+  SET_USER_LOGGED_OUT,
+  SET_USER_LOGIN_FAILURE,
+  SET_USER_LOGIN_SUCCESS,
+} from "../actions/userActions";
+
+const initialState = {
+  user: { name: "", email: "", role_id: "", token: "" },
+  isLoading: false,
+  error: null,
+};
 
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SET_USER":
-      return { ...state, user: action.payload };
+    case SET_USER_LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        isLoading: false,
+        error: null,
+      };
+    case SET_USER_LOGIN_FAILURE:
+      return { ...state, isLoading: false, error: action.payload };
+    case SET_USER_LOGGED_OUT:
+      return { ...state, user: {}, isLoading: false, error: null };
+
     default:
       return state;
   }
