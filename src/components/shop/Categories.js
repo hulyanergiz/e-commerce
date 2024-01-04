ea665d035d693f862452e4dd1e0b3e6da3a228cb
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-04 20:57:07 +0300

    sorts categories and gets first fives & maps them

--- src/components/shop/Categories.js
+++ src/components/shop/Categories.js
@@ -3,9 +3,12 @@ import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
   const categories = useSelector((store) => store.global.categories);
+  const sortedCategories = categories
+    .sort((a, b) => b.rating - a.rating)
+    .slice(0, 5);
   return (
     <div className="flex lg:flex-row w-[73%] m-auto  gap-x-3.5 pb-12 max-lg:flex-col max-lg:gap-y-2 md:max-lg:w-[50%]">
-      {data.shop.categoryCards.map((item, i) => {
+      {sortedCategories.map((item, i) => {
         return <CategoryCard data={item} key={i} />;
       })}
     </div>
