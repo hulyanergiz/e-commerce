80fce7059803a0cd891d8205c065298a2d0c85d3
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-04 21:00:10 +0300

    aligns categoryCard heights-CSS

--- src/components/shop/CategoryCard.js
+++ src/components/shop/CategoryCard.js
@@ -2,7 +2,7 @@ const CategoryCard = (props) => {
   const { title, img } = props.data;
   return (
     <div className="w-full flex justify-center items-center">
-      <img src={img} alt="cloths" className="flex relative w-full" />
+      <img src={img} alt="cloths" className="flex relative w-full h-full" />
       <div className="flex flex-col absolute tracking-wide">
         <p className="text-base text-white font-bold -mb-2">{title}</p>
         <p className="text-sm text-white">5 Items</p>
