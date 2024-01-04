6bc5b1f2e6a919fc7275bd1f1869608f0ccd7a84
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-04 21:45:13 +0300

    adds ratings on categoryCards

--- src/components/shop/CategoryCard.js
+++ src/components/shop/CategoryCard.js
@@ -1,5 +1,5 @@
 const CategoryCard = (props) => {
-  const { title, img, gender } = props.data;
+  const { title, img, gender, rating } = props.data;
   return (
     <div className="w-full h-full flex justify-center items-center cursor-pointer">
       <img src={img} alt="cloths" className="flex relative w-full h-full" />
@@ -9,7 +9,7 @@ const CategoryCard = (props) => {
           {gender === "k" ? "KADIN" : "ERKEK"}
         </p>
         <p className="text-base text-white font-bold -mb-2">{title}</p>
-        <p className="text-sm text-white">5 Items</p>
+        <p className="text-sm text-white">rating:{rating}</p>
       </div>
     </div>
   );
