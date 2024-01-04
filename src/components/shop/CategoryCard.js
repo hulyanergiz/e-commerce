906eb451d98b80e312ebadf4833fa91a2e83c1dd
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-04 21:42:37 +0300

    writes gender notes on categoryCards

--- src/components/shop/CategoryCard.js
+++ src/components/shop/CategoryCard.js
@@ -1,9 +1,13 @@
 const CategoryCard = (props) => {
-  const { title, img } = props.data;
+  const { title, img, gender } = props.data;
   return (
     <div className="w-full flex justify-center items-center">
       <img src={img} alt="cloths" className="flex relative w-full h-full" />
+
       <div className="flex flex-col absolute tracking-wide">
+        <p className="text-base text-white font-bold -mb-2">
+          {gender === "k" ? "KADIN" : "ERKEK"}
+        </p>
         <p className="text-base text-white font-bold -mb-2">{title}</p>
         <p className="text-sm text-white">5 Items</p>
       </div>
