15dcf0d66a681f9aa669474301e0fec77de0999c
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-05 13:11:29 +0300

    moves key to the child of mapped array

--- src/components/shop/Categories.js
+++ src/components/shop/Categories.js
@@ -11,14 +11,14 @@ const Categories = () => {
     <div className="flex lg:flex-row w-[73%] m-auto  gap-x-3.5 pb-12 max-lg:flex-col max-lg:gap-y-2 md:max-lg:w-[50%]">
       {sortedCategories.map((item, i) => {
         return (
-          <div className="w-[19%]">
+          <div className="w-[19%]" key={i}>
             <NavLink
               href={`/shopping/:${
                 item.gender === "k" ? "kadin" : "erkek"
               }/:${item.code.slice(2)}`}
               className="h-full"
             >
-              <CategoryCard data={item} key={i} />
+              <CategoryCard data={item} />
             </NavLink>
           </div>
         );
