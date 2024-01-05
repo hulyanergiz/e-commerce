421491b68abc71718f3ec25ea9ad67522d9a1489
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-05 13:12:50 +0300

    updates setProductList action to fetch products

--- src/store/actions/productActions.js
+++ src/store/actions/productActions.js
@@ -1,13 +1,23 @@
+import { AxiosInstance } from "../../api/api";
+
 export const SET_PRODUCT_LIST = "SET_PRODUCT_LIST";
 export const SET_TOTAL_PRODUCT_COUNT = "SET_TOTAL_PRODUCT_COUNT";
 export const SET_PAGE_COUNT = "SET_PAGE_COUNT";
 export const SET_ACTIVE_PAGE = "SET_ACTIVE_PAGE";
 export const SET_FETCH_STATE = "SET_FETCH_STATE";
 
-export const setProductList = (productList) => ({
-  type: SET_PRODUCT_LIST,
-  payload: productList,
-});
+export const setProductList = () => (dispatch) => {
+  return AxiosInstance.get("/products")
+    .then((res) => {
+      dispatch({
+        type: SET_PRODUCT_LIST,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      console.log("products error", err);
+    });
+};
 export const setTotalProductCount = (totalProductCount) => ({
   type: SET_TOTAL_PRODUCT_COUNT,
   payload: totalProductCount,
