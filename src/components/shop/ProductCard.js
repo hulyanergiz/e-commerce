d26dbbbe29a43b7b3cc335f86a4e64f6ca275a18
Hülya Nergiz <akin.hulya@outlook.com>
2024-02-01 21:53:37 +0300

    css revision in productCard page -adds each product shadow and hover

--- src/components/shop/ProductCard.js
+++ src/components/shop/ProductCard.js
@@ -1,4 +1,6 @@
+import { useDispatch } from "react-redux";
 import { NavLink } from "reactstrap";
+import { addToCart } from "../../store/actions/shoppingCartActions";
 
 const ProductCard = (props) => {
   const {
@@ -14,16 +16,19 @@ const ProductCard = (props) => {
     images,
   } = props.data;
   return (
-    <div className="w-full h-full flex flex-col justify-between items-center py-6">
+    <div className="w-full h-92 flex flex-col justify-between items-center py-6 shadow-md hover:shadow-2xl">
       <NavLink href={`/shop/${category_id}/${id}/${name}`}>
-        <img src={images[0].url} alt="product" className="max-sm:w-[73%]" />
-        <div className="flex flex-col items-center justify-start pt-6 px-6 pb-9 w-[73%] h-72">
-          <p className="text-[#252B42] text-base font-bold">{name}</p>
-          <p className="text-[#737373] text-sm font-bold ">{description}</p>
-          <p className="text-[#BDBDBD] text-base font-bold">
-            {price}{" "}
-            <span className="text-[#23856D] text-base font-bold">{rating}</span>
-          </p>
+        <div className="flex flex-col justify-between items-center h-76">
+          <img src={images[0].url} alt="product" className="max-sm:w-[73%]" />
+          <div className="flex flex-col items-center justify-start pt-6 px-6 pb-9 w-[73%] h-60">
+            <p className="text-[#252B42] text-base font-bold">{name}</p>
+            <p className="text-[#737373] text-sm font-bold ">{description}</p>
+            <p className="text-[#BDBDBD] text-base font-bold">
+              {price}{" "}
+              <span className="text-[#23856D] text-base font-bold">
+                {rating}
+              </span>
+            </p>
 
           {/* <div className="flex flex-row gap-x-1.5">
           <div className={colors[0]}></div>
