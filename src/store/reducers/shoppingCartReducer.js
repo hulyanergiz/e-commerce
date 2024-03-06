aeb4ecd33acd5b4f7c3f423ede519189108eb9cb
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-07 01:55:08 +0300

    edits ADD_TO_CART action type -> if exists count increases, if not exists adds

--- src/store/reducers/shoppingCartReducer.js
+++ src/store/reducers/shoppingCartReducer.js
@@ -10,7 +10,22 @@ const initialState = { cart: [], payment: {}, address: {} };
 export const shoppingCartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      return { ...state, cart: [...state.cart, action.payload] };
+      const existingItemIndex = state.cart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (existingItemIndex !== -1) {
+        const updatedCart = [...state.cart];
+        updatedCart[existingItemIndex].count += 1;
+
+        return { ...state, cart: updatedCart };
+      } else {
+        return {
+          ...state,
+          cart: [...state.cart, { ...action.payload, count: 1 }],
+        };
+      }
+
     case REMOVE_FROM_CART:
       return {
         ...state,
