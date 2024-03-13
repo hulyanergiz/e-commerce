b94fdcf0248fc830999875769b19435e371bf511
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-13 23:48:31 +0300

    maps cart items with only images if cart.length>0. Otherwise writes empty cart message

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,6 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
 const ShoppingCart = () => {
+  const cart = useSelector((store) => store.shoppingCart.cart);
+  const dispatch = useDispatch();
   return (
     <>
+      <h2> SEPETİM - {totalItemCount} ürün</h2>
+      {cart.length > 0 ? (
+        <div className="flex flex-col">
+          <p className="text-[#737373]">
+            Sepetindeki ürünleri Bireysel veya Kurumsal Fatura Seçerek
+            alabilirsin.
+          </p>
+          {cart.map((item, index) => (
+            <div key={index} className="shadow-sm">
+                <img src={item.images[0].url} className="w-24 py-1 pl-2" />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Sepetin boş</p>
+      )}
     </>
   );
 };
