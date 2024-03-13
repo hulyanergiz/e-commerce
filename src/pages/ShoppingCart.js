b34fa61657273b3f6b87caddc394d6ab8d6b8a75
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-13 23:50:30 +0300

    adds other necessary informations about cart items in ShoppingCart page

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -13,7 +13,16 @@ const ShoppingCart = () => {
           </p>
           {cart.map((item, index) => (
             <div key={index} className="shadow-sm">
+              <div className="flex flex-row ">
                 <img src={item.images[0].url} className="w-24 py-1 pl-2" />
+                <div className="flex flex-col justify-between py-[6px]">
+                  <p className="text-[#737373]">{item.name}</p>
+                  <p>
+                    <strong>39 dakika</strong> içinde sipariş verirsen{" "}
+                    <strong>en geç yarın</strong> kargoda!
+                  </p>
+                </div>
+                </div>
             </div>
           ))}
         </div>
