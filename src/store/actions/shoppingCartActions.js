d73702ac60bf113d509e996292b39cf326634501
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-15 01:08:50 +0300

    adds checkItem action to shoppingCartActions

--- src/store/actions/shoppingCartActions.js
+++ src/store/actions/shoppingCartActions.js
@@ -2,6 +2,7 @@ export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const INCREASE_ITEM_COUNT = "INCREASE_ITEM_COUNT";
 export const DECREASE_ITEM_COUNT = "DECREASE_ITEM_COUNT";
+export const CHECK_ITEM = "CHECK_ITEM";
 export const SET_PAYMENT = "SET_PAYMENT";
 export const SET_ADDRESS = "SET_ADDRESS";
 
@@ -16,6 +17,10 @@ export const increaseItemCount = (productId) => ({
 });
 export const decreaseItemCount = (productId) => ({
   type: DECREASE_ITEM_COUNT,
+  payload: productId,
+});
+export const checkItem = (productId) => ({
+  type: CHECK_ITEM,
   payload: productId,
 });
 export const setPayment = (payment) => ({
