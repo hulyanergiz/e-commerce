30104aa664c80f9046731e21e1f8d98cf0af9ad4
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-22 00:04:20 +0300

    adds icon for yeni adress ekle button

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,3 +1,5 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 const Order = () => {
@@ -36,6 +38,7 @@ const Order = () => {
           onClick={() => setOpenForm(!openForm)}
           className="w-1/2 flex flex-row justify-center items-center gap-x-3 shadow-[-3px_-3px_3px_3px_rgba(0,0,0,0.1)] rounded-lg"
         >
+          <FontAwesomeIcon size="md" icon={faPlus} color="#23A6F0" />
           <p className="pt-[1rem]">Yeni Adres Ekle</p>
         </button>
   );
