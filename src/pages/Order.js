844fba73c9d1e1e83341a17aae6f292fd3e95136
Hülya Nergiz <akin.hulya@outlook.com>
2024-03-22 23:17:01 +0300

    adds form template to be able to add new address

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -54,6 +54,56 @@ const Order = () => {
           <FontAwesomeIcon size="md" icon={faPlus} color="#23A6F0" />
           <p className="pt-[1rem]">Yeni Adres Ekle</p>
         </button>
+        {openForm && (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className=" w-3/4 mx-auto absolute bg-[#fff]"
+          >
+            <div className="bg-[#fff] flex flex-col items-center py-4 shadow-[-3px_-3px_3px_3px_rgba(0,0,0,0.1)] rounded-lg">
+              <h4 className="text-center">Yeni Adres Ekle</h4>
+              <label htmlFor="addressTitle" className="w-[73%] text-start py-1">
+                Adres Başlığı:
+              </label>
+              <input
+                id="addressTitle"
+              />
+              <div className="w-[73%] flex flex-row justify-between">
+                <div className="flex flex-col">
+                  <label htmlFor="name" className="w-[73%] text-start py-1">
+                    Ad:
+                  </label>
+                  <input
+                    id="name"
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="surname" className="w-[73%] text-start py-1">
+                    Soyad:
+                  </label>
+                  <input
+                    id="surname"
+                  />
+                </div>
+              </div>
+
+              <label htmlFor="phone" className="w-[73%] text-start py-1">
+                Cep telefonu:
+              </label>
+              <input
+                id="phone"
+              />
+              <label
+                htmlFor="addressDetails"
+                className="w-[73%] text-start py-1"
+              />
+              <input
+                type="text"
+              />
+            </div>
+          </form>
+        )}
+      </div>
+    </div>
   );
 };
 export default Order;
