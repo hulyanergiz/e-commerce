0610044b20b4ff1fe815a616768652482fa5a375
Hülya Nergiz <akin.hulya@outlook.com>
2024-04-04 00:17:25 +0300

    edits get request for /verify

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PageContent from "./layouts/PageContent";
 import Footer from "./layouts/Footer";
 import { useDispatch, useSelector } from "react-redux";
 import { AxiosInstance, renewAxiosInstance } from "./api/api";
-import { setUserSuccess } from "./store/actions/userActions";
+import { setUserSuccess, tokenVerify } from "./store/actions/userActions";
 import { useEffect } from "react";
 import { useLocalStorage } from "./hooks/useLocalStorage";
 import { useHistory } from "react-router-dom";
@@ -15,14 +15,20 @@ function App() {
   const { user } = useSelector((store) => store.user.user);
   const dispatch = useDispatch();
   const history = useHistory();
-  const [token, setToken] = useLocalStorage("token", "");
-
+  //const [token, setToken] = useLocalStorage("token", "");
   useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("token"));
     if (token) {
-      AxiosInstance.get("/verify")
+      AxiosInstance.get("/verify", {
+        headers: {
+          Authorization: token,
+        },
+      })
         .then((res) => {
+          console.log("token", token);
           console.log("verify", res.data);
           dispatch(setUserSuccess(res.data));
+          dispatch(tokenVerify(res.data.token));
         })
         .catch((err) => {
           console.error("verify", err);
@@ -33,7 +39,7 @@ function App() {
     }
     dispatch(setCategories());
     dispatch(setProductList());
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="App">
