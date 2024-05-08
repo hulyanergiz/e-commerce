519cadda17fdaebcc8aef5a78027d245a853fcf9
Hülya Nergiz <akin.hulya@outlook.com>
2024-05-09 00:05:59 +0300

    adds case SELECT_ADDRESS in orderReducer

--- src/store/reducers/orderReducer.js
+++ src/store/reducers/orderReducer.js
@@ -1,6 +1,14 @@
-const initialState = {};
+import {
+  SELECT_ADDRESS,
+} from "../actions/orderActions";
+const initialState = { cart: [], address: [], card: {} };
 export const orderReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SELECT_ADDRESS:
+      return {
+        ...state,
+        address: [state.address, action.payload],
+      };
     default:
       return state;
   }
